de31df85ebe015d6240791706db7d3151effa7af
muojie <htc.guo@gmail.com>
2019-11-26 18:31:22 +0800

    jni传递字符串参数 设置app读写权限

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -25,6 +25,9 @@
 
 #include <jni.h>
 #include <string>
+#include <android/log.h>
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,"testff",__VA_ARGS__)
+
 extern "C"{
     #include <libavcodec/avcodec.h>
 }
@@ -39,4 +42,26 @@ Java_aplay_testffmpeg_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++ ";
     hello += avcodec_configuration();
     return env->NewStringUTF(hello.c_str());
+}
+extern "C"
+JNIEXPORT jboolean JNICALL
+Java_aplay_testffmpeg_MainActivity_Open(JNIEnv *env, jobject instance, jstring url_,
+                                        jobject handle) {
+    const char *url = env->GetStringUTFChars(url_, 0);
+
+                 // TODO
+    FILE *fp = fopen(url,"rb");
+    if(!fp)
+    {
+        LOGW("File %s open failed!",url);
+    }
+    else
+    {
+        LOGW("File %s open succes!",url);
+        fclose(fp);
+
+    }
+
+    env->ReleaseStringUTFChars(url_, url);
+    return 0;
 }
--- app/src/main/java/aplay/testffmpeg/MainActivity.java
+++ app/src/main/java/aplay/testffmpeg/MainActivity.java
@@ -1,5 +1,8 @@
 package aplay.testffmpeg;
 
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -11,13 +14,36 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary( "native-lib" );
     }
 
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE" };
+
+    public static void verifyStoragePermissions(Activity activity) {
+
+        try {
+            //检测是否有写的权限
+            int permission = ActivityCompat.checkSelfPermission(activity,
+                    "android.permission.WRITE_EXTERNAL_STORAGE");
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // 没有写的权限，去申请写的权限，会弹出对话框
+                ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,REQUEST_EXTERNAL_STORAGE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
 
+        verifyStoragePermissions(this);
+
         // Example of a call to a native method
         TextView tv = (TextView) findViewById( R.id.sample_text );
+        Open( "/sdcard/1080.mp4",this );
         tv.setText( stringFromJNI() );
     }
 
@@ -26,4 +52,5 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public native boolean Open(String url,Object handle);
 }
