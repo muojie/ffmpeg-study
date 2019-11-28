5df6ec9dc91e8c015613b643306a1e0a67d3d576
muojie <htc.guo@gmail.com>
2019-11-28 16:05:24 +0800

    视频播放窗口去掉标题栏、全屏、横屏

--- app/src/main/java/aplay/testffmpeg/MainActivity.java
+++ app/src/main/java/aplay/testffmpeg/MainActivity.java
@@ -25,11 +25,14 @@
 
 package aplay.testffmpeg;
 
+import android.content.pm.ActivityInfo;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -63,6 +66,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
+        //去掉标题栏
+        supportRequestWindowFeature( Window.FEATURE_NO_TITLE);
+        //全屏，隐藏状态
+        getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN ,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        //屏幕为横屏
+        setRequestedOrientation( ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE );
+
+
         setContentView( R.layout.activity_main );
 
         verifyStoragePermissions(this);
